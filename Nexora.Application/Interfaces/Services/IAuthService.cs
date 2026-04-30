098a0dd0960c7bb03d9a902952de87b78ea2c4e0
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-04-30 10:09:25 +0200

    added auth service

---
+++ Nexora.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using Nexora.Application.Users.Commands.Login;
+using Nexora.Application.Users.Commands.Register;
+using Nexora.Application.Users.Commands.UploadAvatar;
+using Nexora.Domain.Constants;
+using Nexora.Domain.DTOs;
+using Nexora.Domain.Exceptions;
+
+namespace Nexora.Application.Interfaces.Services;
+
+public interface IAuthService
+{
+    public Task<RegisterUserResponse> RegisterUserService(RegisterUserCommand request);
+
+    public Task<LoginResponse> LoginUserHandler(LoginUserCommand request);
+
+    public Task<UserDto> RetrieveUser(string id);
+}
--- Nexora.Application/Users/Services/AuthService.cs
+++ Nexora.Application/Users/Services/AuthService.cs
@@ -1,29 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Nexora.Application.Interfaces.Context;
+using Nexora.Application.Interfaces.JwtService;
 using Nexora.Application.Interfaces.Services;
+using Nexora.Application.Users.Commands.Login;
 using Nexora.Application.Users.Commands.Register;
 using Nexora.Application.Users.Commands.UploadAvatar;
 using Nexora.Domain.Constants;
+using Nexora.Domain.DTOs;
 using Nexora.Domain.Exceptions;
+using ValidationException = System.ComponentModel.DataAnnotations.ValidationException;
 
 namespace Nexora.Application.Users.Services;
 
-public class AuthService
+public class AuthService:IAuthService
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IApplicationDbContext _context;
     private readonly IAvatarService _avatarService;
+    private readonly IJwtService _jwtService;
     private ILogger<AuthService> _logger;
 
-    public AuthService(UserManager<ApplicationUser> userManager, IApplicationDbContext context, IAvatarService avatarService, ILogger<AuthService> logger)
+    public AuthService(UserManager<ApplicationUser> userManager, IApplicationDbContext context, IAvatarService avatarService, ILogger<AuthService> logger, IJwtService jwtService)
     {
         _context = context;
         _userManager = userManager;
         _avatarService = avatarService;
         _logger = logger;
+        _jwtService = jwtService;
     }
     
     public async Task<RegisterUserResponse> RegisterUserService(RegisterUserCommand request)
@@ -82,14 +90,60 @@ public class AuthService
             return registerUserResult;
 
     }
+    
+    public async Task<LoginResponse> LoginUserHandler(LoginUserCommand request)
+    {
+        if (request.email.IsNullOrEmpty() || request.password.IsNullOrEmpty())
+        {
+            throw new ValidationException("Required data is missing");
+        }
 
-    public async Task<bool> CheckIfuserExistByEmail(string email)
+        var user = await _userManager.FindByEmailAsync(request.email);
+        if (user == null)
+        {
+            throw new UserIsNotFoundException();
+        }
+
+        var result = await _userManager.CheckPasswordAsync(user, request.password);
+        if (!result)
+        {
+            throw new PasswordIsNotMatched();
+        }
+
+        var token = await _jwtService.CreateToken(user);
+        if (token.IsNullOrEmpty())
+        {
+            throw new BadHttpRequestException("Failed during creating token");
+        }
+
+        return new LoginResponse(token);
+    }
+
+    public async Task<UserDto> RetrieveUser(string id)
+    {
+        if (id.IsNullOrEmpty())
+        {
+            throw new BadHttpRequestException("There is no user`id");
+        }
+
+        var user = await _userManager.Users.Include(u=>u.Avatar).Include(u=>u.Address).FirstOrDefaultAsync(u=>u.Id.Equals(id));
+        if (user == null)
+        {
+            throw new UserIsNotFoundException();
+        }
+
+        return new UserDto(user.Id, user.FirstName + " " + user.LastName, user.Email, user.Avatar.Uri,
+            user.Address?.Line1 + ", "+ user.Address?.City + ", "+user.Address?.Country);
+
+    }
+
+    private async Task<bool> CheckIfuserExistByEmail(string email)
     {
         var result = await _userManager.FindByEmailAsync(email);
         return result == null && false;
     }
 
-    public bool MatchingPasswordHandler(string password, string confirmPassword)
+    private bool MatchingPasswordHandler(string password, string confirmPassword)
     {
         return password.Equals(confirmPassword);
     }
