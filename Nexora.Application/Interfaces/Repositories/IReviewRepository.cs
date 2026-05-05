cf96768f2a666b722219c308a88c54bcb98c4351
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-05-05 09:50:46 +0200

    fixed review repository

--- Nexora.Application/Interfaces/Repositories/IReviewRepository.cs
+++ Nexora.Application/Interfaces/Repositories/IReviewRepository.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Nexora.Application.Review.Request;
+using Nexora.Domain.DTOs;
 
 namespace Nexora.Application.Interfaces.Repositories;
 
 public interface IReviewRepository
 {
-    public Task<IResult> AddReviewToListing(ReviewRequest request);
-    public Task<IResult> AddLikeToReview(Guid reviewId);
-    public Task<IResult> AnswerOnReview(AnswerOnReviewRequest request);
+    Task<bool> CreateAsync(Domain.Entities.Review review);
+    Task<Domain.Entities.Review?> GetByIdAsync(Guid id);
+    Task<IList<Domain.Entities.Review>> GetAllByListingIdAsync(Guid listingId);
+    Task<bool> UpdateAsync(Domain.Entities.Review review);
+    Task<bool> DeleteAsync(Guid id);
+    Task<bool> ExistsAsync(Guid id);
 }
--- Nexora.Infrastructure/Repository/ReviewRepository.cs
+++ Nexora.Infrastructure/Repository/ReviewRepository.cs
@@ -1,39 +1,58 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
+using Microsoft.EntityFrameworkCore;
+using Nexora.Application.Interfaces.Context;
 using Nexora.Application.Interfaces.Repositories;
-using Nexora.Application.Interfaces.Services;
-using Nexora.Application.Review.Request;
-using Nexora.Application.Review.Services;
-using Nexora.Domain.DTOs;
 using Nexora.Domain.Entities;
-
+using Nexora.Domain.Exceptions;
 namespace Nexora.Infrastructure.Repository;
 
 public class ReviewRepository:IReviewRepository
 {
-    
-
-    public ReviewRepository(IReviewService reviewService)
+    private readonly IApplicationDbContext _context;
+    public ReviewRepository(IApplicationDbContext context)
     {
+        _context = context;
     }
-   
-    
-    public async Task<IResult> AddReviewToListing(Review review)
+    public async Task<bool> CreateAsync(Review review)
     {
-        
-        
-    }
-
-    public async Task<IResult> AddLikeToReview(Guid reviewId)
-    {
-        var id = await _reviewService.LikeReview(reviewId);
-        if (id == null) throw new BadHttpRequestException("Failed to like review");
-        return Results.Ok(new {message = "Like was added", data = id});
+        await _context.Reviews.AddAsync(review);
+        var saved = await _context.SaveChangesAsync();
+        return saved > 0;
     }
 
-    public async Task<IResult> AnswerOnReview(AnswerOnReviewRequest request)
+    public async Task<Review?> GetByIdAsync(Guid id)
     {
-        var response = await _reviewService.AnswerOnReview(request);
-        if(response == null)  throw new BadHttpRequestException("Failed to answer on review. Something went wrong");
-        return Results.Ok(new { message = "answers is sent", data = response });
+        Review? review = await _context.Reviews.FirstOrDefaultAsync(r=>r.Id.Equals(id));
+        if (review == null) throw new NotFoundException(nameof(Review), id);
+        return review;
+
+    }
+
+    public async Task<IList<Review>> GetAllByListingIdAsync(Guid listingId)
+    {
+        IList<Review> reviews = await _context.Reviews.Where(r => r.ProductId == listingId).ToListAsync();
+        if (reviews.Count <= 0) throw new NotFoundException(nameof(Review), listingId);
+        return reviews;
+    }
+
+    public async Task<bool> UpdateAsync(Review review)
+    {
+        _context.Reviews.Update(review);
+        var result = await _context.SaveChangesAsync();
+        return result > 0;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        Review? review = await _context.Reviews.FirstOrDefaultAsync(r => r.Id == id);
+        if (review==null) throw new NotFoundException(nameof(Review), id);
+        _context.Reviews.Remove(review);
+        var result = await _context.SaveChangesAsync();
+        return result > 0;
+    }
+
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        return await _context.Reviews.AnyAsync(r=>r.Id==id);
     }
 }
