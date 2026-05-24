136d3547645f93991f173167ad105afa68249833
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-05-24 10:13:47 +0200

    added some loggers to controller

--- Nexora.Api/Controllers/CartController.cs
+++ Nexora.Api/Controllers/CartController.cs
@@ -11,14 +11,17 @@ namespace Nexora.Api.Controllers;
 public class CartController:Controller
 {
     private readonly ICartService _cartService;
-    public CartController(ICartService cartService)
+    private readonly ILogger<CartController> _logger;
+    public CartController(ICartService cartService, Logger<CartController> logger)
     {
         _cartService = cartService;
+        _logger = logger;
     }
     [Authorize]
     [HttpPost]
     public async Task<IResult> AddItemToCart([FromBody] AddingItemToCart request)
     {
+        _logger.LogInformation("Adding item to cart is started");
         var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         return await _cartService.AddListing(request.listingId, id);
     }
@@ -27,6 +30,7 @@ public class CartController:Controller
     [HttpGet]
     public async Task<IResult> GetCart()
     {
+        _logger.LogInformation("Getting cart by user");
         var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         if (id == null) throw new UnauthorizedAccessException();
         return await _cartService.GetCart(id);
@@ -36,6 +40,7 @@ public class CartController:Controller
     [HttpPatch]
     public async Task<IResult> ChangeItemToCart([FromBody] ChangingQuantityRequest request)
     {
+        _logger.LogInformation("Changing quantity of item in cart");
         return await _cartService.ChangeListingQuantity(request);
     }
     
@@ -43,6 +48,7 @@ public class CartController:Controller
     [HttpDelete]
     public async Task<IResult> RemoveItemFromCart([FromQuery] Guid id)
     {
+        _logger.LogInformation("Removing item from cart");
         return await _cartService.RemoveListing(id);
     }
 }
--- Nexora.Api/Controllers/CategoryController.cs
+++ Nexora.Api/Controllers/CategoryController.cs
@@ -10,6 +10,7 @@ namespace Nexora.Api.Controllers;
 public class CategoryController : Controller
 {
     private readonly ICategoryService _categoryService;
+    
     public CategoryController(ICategoryService categoryService)
     {
         _categoryService = categoryService;
